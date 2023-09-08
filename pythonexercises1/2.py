ae447edf90bfe93603223895badba58a15d44478
ngoct <ngocthien0105@gmail.com>
2023-09-08 10:53:01 +0300

    2.3

--- pythonexercises1/2.py
+++ pythonexercises1/2.py
@@ -1,2 +1,13 @@
-user =input('Enter your name:\n')
-print(f'Hello! Nice to meet you, {user}!')
+import math
+
+# user =input('Enter your name:\n')
+# print(f'Hello! Nice to meet you, {user}!')
+
+# radius =float(input('Radius: '))
+# print(f'Area of the circle: {radius*radius*math.pi:.3f}')
+
+
+l =float(input('Length: '))
+w =float(input('Width: '))
+
+print(f'Perimeter of the rectangle: {2*l+2*w}\nArea of the rectangle: {l*w}')
