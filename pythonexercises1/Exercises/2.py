3e5c3f6324350843a1f7c5584fb7dc8e12c591a0
ngoct <ngocthien0105@gmail.com>
2023-09-27 10:06:14 +0300

    2.5

--- pythonexercises1/Exercises/2.py
+++ pythonexercises1/Exercises/2.py
@@ -12,8 +12,9 @@ import math
 #
 # print(f'Perimeter of the rectangle: {2*l+2*w}\nArea of the rectangle: {l*w}')
 
-Num1 =int(input('Enter first number: '))
-Num2 =int(input('Enter second number: '))
-Num3 =int(input('Enter third number: '))
+# Num1 =int(input('Enter first number: '))
+# Num2 =int(input('Enter second number: '))
+# Num3 =int(input('Enter third number: '))
+#
+# print(f'Sum of the numbers: {Num1+Num2+Num3}\nProduct of the numbers: {Num1*Num2*Num3}\nThe average of the numbers: {(Num1+Num2+Num3)/3}')
 
-print(f'Sum of the numbers: {Num1+Num2+Num3}\nProduct of the numbers: {Num1*Num2*Num3}\nThe average of the numbers: {(Num1+Num2+Num3)/3}')
